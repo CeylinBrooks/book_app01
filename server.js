a0171d7564bed16dd2c8b61238f232929abd3255
ceylinbrooks <ceylinbrooks@gmail.com>
2021-01-16 17:18:23 -0600

    Construstor Start

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
-
+app.post('/searches', searchResults)
 
 app.listen(PORT, ()=> {
     console.log(`Now listening on port ${PORT}`)
@@ -19,3 +19,14 @@ function homeHandler(request, response) {
     response.status(200).render('index');
 }
 
+
+//Routes
+
+
+//Constructors
+function Book (obj){
+    this.title =
+    this.author =
+    this.description =
+    this.images =
+}
