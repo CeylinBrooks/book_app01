8d0ca8260c403d0d695838856e8691b163ddf4c2
ceylinbrooks <ceylinbrooks@gmail.com>
2021-01-23 18:09:40 -0600

    Buttons Added

--- server.js
+++ server.js
@@ -60,7 +60,8 @@ function handleError(res) {
 
 // Routes
 app.get('/', homeHandler);
-//app.get('/', )
+app.put('/update/:id', updateBook);
+app.delete('/delete/:id', deleteBook);
 
 
 
@@ -77,6 +78,27 @@ function homeHandler(request, response) {
 function renderTest(request, response) {
     response.render('pages/index.ejs');
 
+}
+
+function updateBook (request, response) {
+    let id = request.params.id;
+    let { author, title, isbn, image_url, description} = request.body;
+    let SQL = 'UPDATE books SET author = $1, title = $2, isbn = $3, image_url = $4, description = $5 WHERE  id = $6;' 
+    let safeValues = [author, title, isbn, image_url, description, id];
+    client.query (SQL, safeValues)
+    .then( ()=> {
+        response.status(200).redirect('/');
+    })
+}
+
+function deleteBook (resquest, response){
+    let id = request.params.id;
+    let SQL = 'DELETE FROM books WHERE id = $1;'
+    let safeValues = [id];
+    client.query(SQL, safeValues)
+    .then(() => {
+        response.status(200).redirect('/');
+    })
 }
 
 function Book(obj) {
